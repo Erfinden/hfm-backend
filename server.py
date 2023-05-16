86d31d93f59248ff3b20445ed1f178a013ed66b4
Neptun <128908840+nanocraftmr@users.noreply.github.com>
2023-05-17 00:32:46 +0200

    new login system

--- server.py
+++ server.py
@@ -4,10 +4,43 @@ import time
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+import bcrypt
+import json
 
 app = Flask(__name__)
+last_upload_time = 0
+USERS_FILE = 'users.json'
+
+# Load users from JSON file
+def load_users():
+    if os.path.exists('users.json'):
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+        return users
+    else:
+        return {}
+
+# Save users to JSON file
+def save_users(users):
+    with open('users.json', 'w') as f:
+        json.dump(users, f, indent=4)
+
+# Delete user folder
+def delete_user_folder(username):
+    folder_path = f'/home/images/{username}'
+    if os.path.exists(folder_path):
+        os.rmdir(folder_path)
+    else:
+        print(f"Folder for user '{username}' does not exist.")
+
+
+users = load_users()
 
 def analyze_image(image_path, result_path):
+    # Check if the username is registered
+    if username not in users:
+        return 'You need to register first!'
+    
     # Load the TFLite model
     interpreter = tf.lite.Interpreter(model_path="my_model.tflite")
     interpreter.allocate_tensors()
@@ -57,27 +90,90 @@ def analyze_image(image_path, result_path):
         f.write(f'Confidence: {confidence}\n')
         f.write(f'{output_str}\n')
         f.write(f'Percentage: {percent:.2f}%\n')
+   
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    file = request.files['image']
     username = request.form['username']  # Assuming the username is sent in the request form
-    current_time = time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())
+    password = request.form['password']  # Assuming the password is sent in the request form
+
+    # Perform password validation
+    if username not in users:
+        return 'You need to register first!'
+    elif not bcrypt.checkpw(password.encode(), users[username].encode()):
+        return 'Invalid username or password.'
+
+    global last_upload_time
+    current_time = time.time()
+    elapsed_time = current_time - last_upload_time
+
+    if elapsed_time < 2:
+        remaining_time = 2 - elapsed_time
+        return f'Please wait for {remaining_time:.2f} seconds before uploading another image.'
+
+    file = request.files['image']
+    current_time_str = time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())
     user_directory = os.path.join('/home/images', username)
 
     # Create the user directory if it doesn't exist
-    if not os.path.exists(user_directory):
-        os.makedirs(user_directory)
+    os.makedirs(user_directory, exist_ok=True)
 
-    file_path = os.path.join(user_directory, '{}.jpg'.format(current_time))
+    file_path = os.path.join(user_directory, f'{current_time_str}.jpg')
     file.save(file_path)
 
     # Perform image analysis
-    result_filename = f'{current_time}.txt'
+    result_filename = f'{current_time_str}.txt'
     result_filepath = os.path.join(user_directory, result_filename)
     analyze_image(file_path, result_filepath)
 
+    last_upload_time = current_time
+
     return 'Image uploaded successfully! Analysis result saved.'
+
+
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+
+    # Load existing users
+    users = load_users()
+
+    if username in users:
+        return 'Username already exists.'
+
+    # Hash the password
+    hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+    # Add the new user to the dictionary
+    users[username] = hashed_password.decode()
+
+    # Create a directory for the new user
+    user_directory = os.path.join('/home/images', username)
+    os.makedirs(user_directory, exist_ok=True)
+
+    # Save the updated user data
+    save_users(users)
+
+    return 'User registered successfully.'
+
+
+# Remove endpoint
+@app.route('/remove', methods=['POST'])
+def remove():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    users = load_users()
+
+    if username not in users or not bcrypt.checkpw(password.encode(), users[username].encode()):
+        return "Invalid username or password."
+
+    delete_user_folder(username)
+    del users[username]
+    save_users(users)
+    return f"User '{username}' deleted successfully."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
